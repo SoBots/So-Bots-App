1614f00034ce87b579b330ba972d54d6d8047f8a
srowly <samrowlands@Sams-MacBook-Pro.local>
2015-02-23 17:42:36 +0000

    Test
    Test

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -27,7 +27,7 @@ public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
     private Vector2 robotPos = new Vector2(0, 0);
 
     private Random random;
-    private Sprite VincentAtanasov;
+    private Sprite KevinJohnston;
 
     private Texture explosionSheet;
     private TextureRegion[] explosionFrames;
