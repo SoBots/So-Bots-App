81e5d4d60c3ac69e0d8bc9cb687adc6f47c1e95f
srowly <samrowlands@Sams-MacBook-Pro.local>
2015-02-25 14:14:58 +0000

    Began new target class.

---
+++ core/src/com/mygdx/game/Target.java
@@ -0,0 +1,24 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * Created by samrowlands on 25/02/15.
+ */
+public class Target
+{
+    private Texture tTexture;
+    private Sprite tSprite;
+
+    private Target()
+    {
+
+    }
+
+    private void render()
+    {
+
+    }
+
+}
