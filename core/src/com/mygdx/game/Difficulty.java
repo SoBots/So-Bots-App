1c31ca3ee0d5597a2058ee011194cdf86c91edc4
Kevin Johnston <kevin.s.johnston@gmail.com>
2015-03-03 11:17:32 +0000

    added difficulty class

---
+++ core/src/com/mygdx/game/Difficulty.java
@@ -0,0 +1,15 @@
+package com.mygdx.game;
+
+/**
+ * Created by kevinjohnston on 03/03/15.
+ */
+public class Difficulty {
+
+    public static int getDifficulty(int score) {
+        //Some code which calculates the number of robots to generate
+        int level
+        if (score<2)
+            level=1;
+        return null
+    }
+}
