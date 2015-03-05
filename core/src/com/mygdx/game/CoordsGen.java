9c8815725ad366d2cc94b610055e12a739042259
Kevin Johnston <kevin.s.johnston@gmail.com>
2015-03-05 14:45:24 +0000

    Fixed Coordinate Generation

--- core/src/com/mygdx/game/CoordsGen.java
+++ core/src/com/mygdx/game/CoordsGen.java
@@ -9,33 +9,27 @@ import java.util.Random;
 
 public class CoordsGen {
 
-
-
     //Generates n sets of x/y co-ordinates.
     public static Vector2[] genCoords(int n,int scrWidth, int scrHeight, int sWidth, int sHeight){
 
         //Vector array which will be returned containing the locations to spawn robots
         Vector2[] coords = new Vector2[n];
 
+        int border = 50;
         //The constraints set by sprite and screen size
-        int minLeft = 50;  //Tolerance on left of screen
-        int maxRight = scrWidth-sWidth-50; //Tolerance on right of screen
-        int minTop = 50;   //Tolerance at top of screen
-        int maxBottom = scrHeight-sHeight-50;//Tolerance at bottom
-        int spaceTol = 50; //Tolerance for space between bot spawns
+        int minLeft = border;  //Tolerance on left of screen
+        int maxLeft = scrWidth - (sWidth + border); //Tolerance on right of screen
+        int maxHeight = border + sHeight;   //Tolerance at top of screen
+        int minHeight = scrHeight - border;//Tolerance at bottom of screen
+        int spaceTol = 4; //Tolerance for space between bot spawns
 
         //Obvious-new random
         Random rn = new Random();
 
-        //Creates vectors to hold planned and already committed locations
-        //Vector2 loc = new Vector2(); //Vector2 of the coordinates generated (not committed)
-        Vector2 locCheck = new Vector2(); //Placeholder vector for checking coordinate suitability
-                                          //with already comitted coordinates.
-
-        boolean success = true; //Determines whether to run loop again to find new coordinates
-
         //First set of coordinates cannot clash, no point in doing this inside while loop
-        coords[0]= new Vector2((float) rn.nextInt((maxRight - minLeft) + 1) + minLeft, (float) rn.nextInt((maxBottom - minTop) + 1) + minTop);
+        float x = (float) rn.nextInt(maxLeft - minLeft + 1) + minLeft;
+        float y = (float) rn.nextInt(minHeight - maxHeight + 1) + maxHeight;
+        coords[0]= new Vector2(x,y);
 
         //iterator for the while loop, starts at one because first set already determined
         int i = 1;
@@ -43,55 +37,40 @@ public class CoordsGen {
         //Have to define these seperately
         float rand1;
         float rand2;
+        Vector2 oldVect;
+        boolean success = true;
 
         while (i<n) {
 
             //Sets a possible location for the next coordinates
-            rand1 = (float)rn.nextInt((maxRight - minLeft) + 1) + minLeft;
-            rand2 = (float)rn.nextInt((maxBottom - minTop) + 1) + minTop;
-            Vector2 loc = new Vector2(rand1, rand2);
+            rand1 = (float) rn.nextInt(maxLeft - minLeft + 1) + minLeft;
+            rand2 = (float) rn.nextInt(minHeight - maxHeight + 1) + maxHeight;
+            Vector2 newVect = new Vector2(rand1, rand2);
 
             //Iterates through already committed coordinates to check for clashes
             for (int j = 0; j < i; j++) {
-                locCheck = coords[j];
+
+                //Coordinate vector
+                oldVect = coords[j];
+
                 //If the new coordinates fall within the area of a previous one, the numbers aren't
                 //suitable. This part checks both x and y domain.
-                if ((loc.x>(locCheck.x-spaceTol) && loc.x<(locCheck.x+sWidth+spaceTol))||(loc.y>(locCheck.y-spaceTol) && loc.y<(locCheck.y+sHeight+spaceTol))) {
+                if (((newVect.x > (oldVect.x - (sWidth + spaceTol)) && newVect.x < (oldVect.x + (sWidth + spaceTol))) &&
+                     (newVect.y > (oldVect.y - (sHeight + spaceTol)) && newVect.y < (oldVect.y + (sHeight + spaceTol) )))) {
                     success = false;
+                    break;
                 }
-                else success=true;
-
+                else success = true;
             }
 
             //If boolean hasn't been set to false, the coordinates were correct and are therefore
             //committed to the coordinate array for robot generation
             if (success) {
-                coords[i] = loc;
+                coords[i] = newVect;
                 i++;
             }
-            //However if !successful, the coordinates are removed
-
-
         }
 
-
-
-          /*public void getRobotLoc(){
-        float minX = robotSprite.getWidth();
-        float maxX = Gdx.graphics.getWidth()-robotSprite.getWidth();
-        float minY = robotSprite.getHeight();
-        float maxY = Gdx.graphics.getHeight()-robotSprite.getHeight();
-        float x=random.nextFloat() * (maxX-minX) + minX;
-        float y=random.nextFloat() * (maxY - minY) + minY;
-        robotPos = new Vector2(x, y);
-    }*/
-        //EX case: Screen height:500, width:200, Sprite height:30, width:30
-        //In order to do this, take the width and height of screen minus a tolerance for each, then
-        //generate the first set of coordinates. Then attempt to create additional coordinates
-        //each time checking that it does not fall into +35(sprite width/height + tolerance), or -5
-        //(for the tolerance on the other side of the sprite. This means that not two sprites will
-        //appear within 5 pixels of each other. If a set of coordinates violates this, a new set will
-        //be generated.
         return coords;
     }
 }
