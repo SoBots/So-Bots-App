78ca82128dc81650ad41dafff3f9291bf5f13283
srowly <srowlands@hotmail.co.uk>
2015-03-17 14:26:48 +0000

    Sorted the touch problem.

--- core/src/com/mygdx/game/Touch.java
+++ core/src/com/mygdx/game/Touch.java
@@ -20,7 +20,7 @@ public class Touch extends InputAdapter {
 
     @Override
     //Gets the x/y co-ordinates on screen of player touch.
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
         touchPoint = new Vector3();
         camera.unproject(touchPoint.set(screenX, screenY, 0));
         mainGame.setCos(new Vector2(touchPoint.x, touchPoint.y));
